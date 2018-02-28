b50094c9e89dd402c703d2a2836135866e1d3aab
Ignas Grigaravicius <kickeris@gmail.com>
2018-02-28 12:23:58 +0000

    home and accounts app added, index html added, urls updated, bootsrtap linked

---
+++ accounts/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render
+
+# Create your views here.
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            user = auth.authenticate(username=request.POST.get('username_or_email'),
+                                     password=request.POST.get('password'))
+
+            if user is not None:
+                auth.login(request, user)
+                messages.error(request, "You have successfully logged in")
+
+                if request.GET and request.GET['next'] !='':
+                    next = request.GET['next']
+                    return HttpResponseRedirect(next)
+                else:
+                    return redirect(reverse('login'))
+            else:
+                form.add_error(None, "Your username or password was not recognised")
+    else:
+        form = UserLoginForm()
+
+    args = {'form': form, 'next': request.GET['next'] if request.GET and 'next' in request.GET else ''}
+    args.update(csrf(request))
+    return render(request, 'login.html', args)
+
+def logout(request):
+    auth.logout(request)
+    messages.success(request, 'You have successfully logged out')
+    return redirect(reverse('index'))
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            user = auth.authenticate(username=request.POST.get('username'),
+                                     password=request.POST.get('password1'))
+
+            if user:
+                auth.login(request, user)
+                messages.success(request, "You have successfully registered")
+                return redirect(reverse('profile'))
+
+            else:
+                messages.error(request, "unable to log you in at this time!")
+
+    else:
+        form = UserRegistrationForm()
+
+    args = {'form': form}
+    args.update(csrf(request))
+
+    return render(request, 'profile.html', args)
--- printLab/urls.py
+++ printLab/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
+from home.views import get_index
 
 urlpatterns = [
+    url(r'^$', get_index, name='index'),
     path('admin/', admin.site.urls),
+    
 ]
