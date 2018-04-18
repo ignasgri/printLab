6e2a7860b004e53c339a6d158a31819071e2dce2
Ignas Grigaravicius <kickeris@gmail.com>
2018-04-18 23:31:05 +0100

    page created, urls updated, views added

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-from .views import main_dashboard
+from .views import main_dashboard, a4
 
 urlpatterns = [
     # url(r'^dashboard/$', dashboard_view),
     url(r'^main/$', main_dashboard, name='main_dashboard'),
-
+    url(r'^main/a4$', a4, name='a4'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages, auth
 from django.template.context_processors import csrf
 from django.contrib.auth.decorators import login_required
 
-@login_required(login_url='/accounts/login') #redirect when user is not logged in
+@login_required(login_url='/') #redirect when user is not logged in
 def main_dashboard(request):
     if request.method == 'POST':
         form = UserLoginForm(request.POST)
@@ -30,3 +30,29 @@ def main_dashboard(request):
     args = {'form': form, 'next': request.GET['next'] if request.GET and 'next' in request.GET else ''}
     args.update(csrf(request))
     return render(request, 'dashboard.html', args)
+
+@login_required(login_url='/') #redirect when user is not logged in
+def a4(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            user = auth.authenticate(username=request.POST.get('username_or_email'),
+                                     password=request.POST.get('password'))
+
+            if user is not None:
+                auth.login(request, user)
+                messages.error(request, "You have successfully logged in")
+
+                if request.GET and request.GET['next'] !='':
+                    next = request.GET['next']
+                    return HttpResponseRedirect(next)
+                else:
+                    return redirect(reverse('a4'))
+            else:
+                form.add_error(None, "Your username/email or password was not recognised")
+    else:
+        form = UserLoginForm()
+
+    args = {'form': form, 'next': request.GET['next'] if request.GET and 'next' in request.GET else ''}
+    args.update(csrf(request))
+    return render(request, 'a4.html', args)
