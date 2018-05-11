007ad8e7d236d58b49d7c68f84dcac9f29c5d838
Ignas Grigaravicius <kickeris@gmail.com>
2018-05-11 10:03:40 +0100

    fitty js added to fit text dinamically, js config for fitty, css updated, radio buttons added for kg or each

--- static/js/text.js
+++ static/js/text.js
@@ -1,3 +1,8 @@
+fitty('#option',{
+    minSize: 80,
+    maxSize: 100,
+    multiLine: true,
+});
 var selectElem = document.getElementById('options')
 var pElem = document.getElementById('option')
 
@@ -20,7 +25,12 @@ selectElement.addEventListener('change', function() {
 
 })
 
-
+fitty('#a4_description_result',{
+    minSize: 30,
+    maxSize: 45,
+    multiLine: true,
+    // observeMutations: true,
+});
 $(document).ready(function () {
     $('#a4_description').keyup(function(e){
         var keyed = $(this).val().replace(/[]/g, '');
@@ -35,28 +45,18 @@ $(document).ready(function () {
         $("#a4_description_result").html(keyed);
      });
     });
+// fitty('#a4_description_result',{
+//     minSize: 16,
+//     maxSize: 256,
+//     multiLine: true,
+//     observeMutations: true
+//     });
 
 function insertText(elemID, text)
 {
     var elem = document.getElementById('a4_weight_result');
     elem.innerHTML += $("#a4_weight").val();
 }
-
-// 
-// $(document).ready(function () {
-//     $('#a4_weight').keyup(function(e){
-//         var keyed = $(this).val().replace(/[]/g, '');
-//         if(keyed == '<')
-//         {
-//             $('#a4_weight').keyup(function(e){
-//                 var keyed = $(this).val().replace(/[]/g,'');
-//                 $('#a4_weight_result"').html('<' + keyed);
-//             });
-//         }
-//         else
-//         $("#a4_weight_result").html(keyed);
-//         });
-//     });
 
 $(document).ready(function () {
     $('#title4').keyup(function(e){
@@ -119,20 +119,6 @@ $(document).ready(function () {
         });
     });
     
-// $(document).ready(function () {
-//     $('#title8').keyup(function(e){
-//         var keyed = $(this).val().replace(/[]/g, '');
-//         if(keyed == '<')
-//         {
-//             $('#title8').keyup(function(e){
-//                 var keyed = $(this).val().replace(/[]/g,'');
-//                 $('#result8"').html('<' + keyed);
-//             });
-//         }
-//         else
-//         $("#result8").html(keyed);
-//         });
-//     });
 var currentDate = new Date()
 var day = currentDate.getDate()
 var month = currentDate.getMonth() + 1
@@ -145,9 +131,4 @@ function myFunction() {
     document.getElementById("result8").innerHTML = x;
 }
 
-// $(document).ready(function () {
-//     $('#title').keyup(function(e){
-//         var keyed = $(this).val().replace(/[]/g, '');
-//     $("#result").html(keyed);
-//     });
-// });
+
